baf1aefeab3abdf8c723490ca45465169e4f6219
joelorzet <joelorzt@gmail.com>
2022-12-07 17:19:19 -0300

    added a module folder

---
+++ src/module/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TasksModule } from './tasks/tasks.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      autoLoadEntities: true,
+      synchronize: false,
+    }),
+    TasksModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
